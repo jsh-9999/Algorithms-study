84d770821f07098c6ddc4b25bdccd01c0a5d644e
jsh-9999 <60505773+jsh-9999@users.noreply.github.com>
2022-07-20 22:35:13 +0900

    third message

--- Test02.py
+++ Test02.py
@@ -43,14 +43,19 @@
 
 
 
-h = int(input())
+# h = int(input())
+#
+# count=0
+#
+# for i in range(h+1):
+#     for j in range(60):
+#         for k in range(60):
+#             if '3' in str(i) + str(j) + str(k):
+#                 count+=1
+#
+# print(count)
 
-count=0
 
-for i in range(h+1):
-    for j in range(60):
-        for k in range(60):
-            if '3' in str(i) + str(j) + str(k):
-                count+=1
 
-print(count)
+
+
