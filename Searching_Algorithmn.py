e748a16496d896c4d941a4e6110023a5788f00d2
jsh-9999 <60505773+jsh-9999@users.noreply.github.com>
2022-07-21 22:29:08 +0900

    dfs bfs study

--- Searching_Algorithmn.py
+++ Searching_Algorithmn.py
@@ -47,34 +47,102 @@
 
 
 
+# from collections import deque
+#
+# def bfs(graph, start, visited):
+#     queue = deque([start])
+#
+#     visited[start] = True
+#
+#     while queue:
+#         v= queue.popleft()
+#         print(v, end=' ')
+#
+#         for i in graph[v]:
+#             if not visited[i]:
+#                 queue.append(i)
+#                 visited[i] = True
+#
+# graph = [
+#     [],
+#     [2, 3, 8],
+#     [1, 7],
+#     [1, 4, 5],
+#     [3, 5],
+#     [3, 4],
+#     [7],
+#     [2, 6, 8],
+#     [1, 7]
+# ]
+# visited = [False] * 9
+# bfs(graph, 1, visited)
+#
+
+
+
+# n, m=map(int, input().split())
+#
+# graph = []
+#
+# for i in range(n):
+#     graph.append(map(list(int, input())))
+#
+#
+# def dfs(x, y):
+#
+#     if x<=-1 or y>=m or y<=-1 or x>=n:
+#         return False
+#     if graph[x][y] == 0:
+#         graph[x][y] ==1
+#
+#         dfs(x-1, y)
+#         dfs(x+1, y)
+#         dfs(x,y-1)
+#         dfs(x,y+1)
+#
+#         return True
+#     return False
+#
+# result = 0
+# for i in range(n):
+#     for j in range(m):
+#         if dfs(i,j) == True:
+#             result+=1
+
+
 from collections import deque
 
-def bfs(graph, start, visited):
-    queue = deque([start])
+n, m = map(int, input().split())
 
-    visited[start] = True
+graph = []
+for i in range(n):
+    graph.append(list(map(int, input())))
+
+
+dx = [-1, 1, 0, 0]
+dy = [0,0,-1,1]
+
+def bfs(x,y):
+
+    queue = deque()
+    queue.append((x,y))
 
     while queue:
-        v= queue.popleft()
-        print(v, end=' ')
+        x,y = queue.popleft()
 
-        for i in graph[v]:
-            if not visited[i]:
-                queue.append(i)
-                visited[i] = True
+        for i in range(4):
+            nx = x+dx[i]
+            ny = y+dy[i]
 
-graph = [
-    [],
-    [2, 3, 8],
-    [1, 7],
-    [1, 4, 5],
-    [3, 5],
-    [3, 4],
-    [7],
-    [2, 6, 8],
-    [1, 7]
-]
-visited = [False] * 9
-bfs(graph, 1, visited)
+            if nx < 0 or ny <0 or nx>=n or ny >=m:
+                continue
+            if graph[nx][ny]==0:
+                continue
+            if graph[nx][ny]==1:
+                graph[nx][ny] = graph[x][y] +1
+                queue.append((nx,ny))
 
+        return graph[n-1][m-1]
+
+print(bfs(0,0))
 
